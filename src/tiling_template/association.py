edcc651e0aa707f2aa8a0e7c98e10d6e0b0a1e40
Sandy Kerr <sandykerr20@gmail.com>
2026-09-15 14:47:02 -0400

    fixing association small bug

--- src/tiling_template/association.py
+++ src/tiling_template/association.py
@@ -38,7 +38,7 @@ class FilenameAssociationStrategy(AssociationStrategy):
         # Conditional prefix removal
         if self.prefix_to_remove is not None:
             key_candidate_old = key_candidate
-            key_candidate = key_candidate.removeprefix()
+            key_candidate = key_candidate.removeprefix(self.prefix_to_remove)
             if key_candidate_old == key_candidate:
                 print(
                     f"[WARNING]: Prefix was not removed from filename. "
@@ -48,7 +48,7 @@ class FilenameAssociationStrategy(AssociationStrategy):
         # Conditional suffix removal
         if self.suffix_to_remove is not None:
             key_candidate_old = key_candidate
-            key_candidate = key_candidate.removesuffix()
+            key_candidate = key_candidate.removesuffix(self.suffix_to_remove)
             if key_candidate_old == key_candidate:
                 print(
                     f"[WARNING]: Suffix was not removed from filename. "
